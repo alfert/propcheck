cd24d9c8af186b68424668cb210eb7b81b49ca46
Klaus Alfert <klaus.alfert@googlemail.com>
2015-07-05 09:48:02 +0200

    property macro (taken from properex)

---
+++ lib/properties.ex
@@ -0,0 +1,21 @@
+defmodule PropCheck.Properties do
+    defmacro __using__(_) do
+        quote do
+            import Proper
+            import Proper.Properties
+            import :proper_types, except: [lazy: 1, to_binary: 1, function: 2]
+        end
+    end
+    
+    defmacro property(name, opts) do
+        case name do
+            {name, _, _} ->
+                prop_name = :"prop_#{name}"
+            name when is_atom(name) or is_binary(name) or is_list(name) ->
+                prop_name = :"prop_#{name}"
+        end
+        quote do
+            def unquote(prop_name)(), unquote(opts)
+        end
+    end
+end
