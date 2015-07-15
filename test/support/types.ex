93da75f546a990c959e133efa03d592f11025280
Klaus Alfert <klaus.alfert@googlemail.com>
2015-07-15 10:03:07 +0200

    various type definitions for testing purposes

---
+++ test/support/types.ex
@@ -0,0 +1,16 @@
+defmodule PropCheck.Test.Types do
+	@moduledoc """
+	This module contains various type definitions to check that the generator generation
+	works properly. 
+	"""
+	use PropCheck.TypeGen
+
+	@type my_numbers :: integer
+	@type my_small_numbers :: 0..100
+	@type yesno :: :yes | :no
+
+	@type my_list(t) :: [t]
+	@type safe_stack(t) :: {pos_integer, list(t)}
+
+	@opaque tree(t) :: :leaf | {:node, t, tree(t), tree(t)}
+end
