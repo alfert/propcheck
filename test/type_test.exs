f088f5e89b65b69d2793cf6dd19b9eea3d6186d4
Klaus Alfert <klaus.alfert@googlemail.com>
2015-08-04 18:33:36 +0200

    testing parts of the structure of type expressions

--- test/type_test.exs
+++ test/type_test.exs
@@ -1,6 +1,6 @@
 defmodule PropCheck.Test.TypeTest do
 	use ExUnit.Case
-
+	alias PropCheck.Type.TypeExpr
 	test "all types available" do
 		types = PropCheck.Test.Types
 			|> PropCheck.TypeGen.defined_types
@@ -19,6 +19,13 @@ defmodule PropCheck.Test.TypeTest do
 		assert [:t] == ps
 
 		IO.inspect e
-	end
+		%TypeExpr{constructor: :union, args: u_args} = e
+		# Problem: node(t) must be a :ref and not be a :literal
+		cs = u_args |> Enum.map fn %TypeExpr{constructor: c} -> c end
+		assert cs |> Enum.any? &(&1 == :literal) # this is the :leaf part
+		assert cs |> Enum.any? &(&1 == :tuple) # this is the node part
+		
+		
+	end 
 	
 end
