07bae24284a469062b8f5f6d4e48b0ed0eea9e13
Klaus Alfert <klaus.alfert@gmail.com>
2016-05-21 18:33:14 +0200

    added a corrected delete function and used it in the property.

--- test/support/tree.ex
+++ test/support/tree.ex
@@ -1,6 +1,6 @@
 defmodule PropCheck.Test.Tree do
 	@moduledoc """
-	The tree implementation of 2013 tutorial, Elixir version. 
+	The tree implementation of 2013 tutorial, Elixir version.
 	"""
 
 	use PropCheck.Properties
@@ -13,13 +13,13 @@ defmodule PropCheck.Test.Tree do
 	def member({:node, _, left, right}, x) do
 		member(left, x) or member(right, x)
 	end
-	
+
 	@spec join(tree(t), tree(t)) :: tree(t) when t: var
 	def join(:leaf, t2), do: t2
-	def join({:node, x, l1, r1}, t2) do 
+	def join({:node, x, l1, r1}, t2) do
 		{:node, x, join(l1, r1), t2}
 	end
-	
+
 	@doc """
 	Bad delete implementation: doesn't delete any occurence of X that is
 	nested under some other node containing X
@@ -28,6 +28,12 @@ defmodule PropCheck.Test.Tree do
 	def delete(:leaf, _), do: :leaf
 	def delete({:node, x, l, r}, x), do: join(l, r)
 	def delete({:node, y, l, r}, x), do: {:node, y, delete(l, x), delete(r, x)}
+
+	@doc "Corrected delete implementation"
+	@spec delete2(tree(t), t) :: tree(t) when t: var
+	def delete2(:leaf, _), do: :leaf
+	def delete2({:node, x, l, r}, x), do: join(delete2(l, x), delete2(r,x))
+	def delete2({:node, y, l, r}, x), do: {:node, y, delete2(l, x), delete2(r, x)}
 
 	@spec tree_sum(tree(number)) :: number
 	def tree_sum(:leaf), do: 0
@@ -38,11 +44,12 @@ defmodule PropCheck.Test.Tree do
 	def pre_order({:node, x, l, r}), do: [x] ++ pre_order(l) ++ pre_order(r)
 
 	################################
-	### Properties of the tree 
+	### Properties of the tree
 
+	# delete is faulty, therefore we expect it fail now and then
 	property "delete" do
 		forall {x, t} in {integer, tree(integer)} do
-			not member(delete(t, x), x)
+			not member(delete2(t, x), x)
 		end
 	end
 
@@ -96,7 +103,7 @@ defmodule PropCheck.Test.Tree do
     def tree4(0, _), do: :leaf
     def tree4(s, g), do:
     	frequency [
-    		{1, tree4(0, g)}, 
+    		{1, tree4(0, g)},
     		{9, lazy {:node, g, tree4(s, g), tree4(s, g)}}
     	]
 
@@ -107,7 +114,7 @@ defmodule PropCheck.Test.Tree do
     def tree5(0, _), do: :leaf
     def tree5(s, g), do:
     	frequency [
-    		{1, tree5(0, g)}, 
+    		{1, tree5(0, g)},
     		{9, lazy {:node, g, tree5(div(s, 2), g), tree5(div(s, 2), g)}}
     	]
 
@@ -119,7 +126,7 @@ defmodule PropCheck.Test.Tree do
     def tree6(0, _), do: :leaf
     def tree6(s, g), do:
     	frequency [
-    		{1, tree6(0, g)}, 
+    		{1, tree6(0, g)},
     		{9, letshrink([l, r] = [tree6(div(s, 2), g), tree6(div(s, 2), g)]) do
     				{:node, g, l, r}
     			end
