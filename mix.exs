c0c71c1ca06ae500186ed126bdd87a38975b2f69
Klaus Alfert <klaus.alfert@gmail.com>
2016-07-21 07:19:36 +0200

    Better package description

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Propcheck.Mixfile do
   defp description do
     """
     PropCheck provides property based testing and is an Elixir layer around
-    PropEr. It tries to be compatible to Quuvic's QuickCheck Elixir library.
+    PropEr. It is also inspired by Quuvic's QuickCheck Elixir library.
     """
   end
 
