e99d87f0ec627167e5f394db346ee01fe7ea806b
Klaus Alfert <klaus.alfert@gmail.com>
2017-08-30 08:13:07 +0200

    Run CounterStrike as part of the Application.

--- lib/app.ex
+++ lib/app.ex
@@ -2,7 +2,7 @@ defmodule PropCheck.App do
   use Application
 
   @moduledoc false
-  
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -11,6 +11,7 @@ defmodule PropCheck.App do
     children = [
       # Define workers and child supervisors to be supervised
       # worker(Propcheck.Worker, [arg1, arg2, arg3])
+      worker(PropCheck.CounterStrike, ["counterexamples.dets", [name: PropCheck.CounterStrike]])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
