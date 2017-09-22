5bf8a166b9c00e2488f119d2e08825f8661a9801
Klaus Alfert <klaus.alfert@gmail.com>
2017-09-23 00:06:04 +0200

    Unused arguments renamed

--- lib/mix.ex
+++ lib/mix.ex
@@ -29,7 +29,7 @@ defmodule Mix.Tasks.Propcheck do
     def run(_args) do
       filename = PropCheck.Mix.counter_example_file() |> String.to_charlist()
       {:ok, ctx} = :dets.open_file(filename)
-      fn {{m,f,a}, counter_example}, counter ->
+      fn {{m, f, _a}, counter_example}, counter ->
         prop = "#{m}.#{f}()"
         Mix.Shell.IO.info "##{counter}: Property #{prop}: #{inspect counter_example}"
         counter + 1
