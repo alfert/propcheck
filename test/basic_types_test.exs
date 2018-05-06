d8e5cb32212a7afc1eeb5f484ee8c83d92318e87
Klaus Alfert <klaus.alfert@gmail.com>
2018-05-06 16:31:13 +0200

    A test using symbolic auto calls.

--- test/basic_types_test.exs
+++ test/basic_types_test.exs
@@ -3,4 +3,22 @@ defmodule PropCheck.Test.BasicTypes do
   use PropCheck
 
   doctest(PropCheck.BasicTypes)
+
+  #########
+  # Symbolic calls
+  # as explained in http://propertesting.com/book_custom_generators.html#_symbolic_calls
+  # work also in PropCheck.
+  def dict_autosymb(), do:
+    sized(size, dict_autosymb(size, {:"$call", :dict, :new, []}))
+  def dict_autosymb(0, dict), do: dict
+  def dict_autosymb(n, dict), do:
+    dict_autosymb(n-1, {:"$call", :dict, :store, [integer(), integer(), dict]})
+
+  property "symbolic auto calls on dict - expected to fail" do
+    forall d <- dict_autosymb() do
+      :dict.size(d) < 5
+    end
+    |> fails
+  end
+
 end
