42ded970820ce6265faff8ae7192e4b4f509f47b
Klaus Alfert <klaus.alfert@gmail.com>
2018-06-24 10:24:44 +0200

    Correct specs and allow incrementing by more than 1

--- test/support/counter.ex
+++ test/support/counter.ex
@@ -7,13 +7,15 @@ defmodule PropCheck.Test.Counter do
   """
 
   use Agent
-
+  require Logger
   @type reset_t :: pos_integer | :infinity
 
   @spec start_link() :: {:ok, pid}
   @spec start_link(atom) :: {:ok, pid}
-  @spec start_link(atom) :: {:ok, pid}
-  def start_link(reset \\:infinity, name \\ __MODULE__) do
+  @spec start_link(reset_t, atom) :: {:ok, pid}
+  def start_link(reset \\:infinity, name \\ __MODULE__) when is_atom(name) and
+    (reset == :infinity or is_integer(reset))
+  do
     Agent.start_link(fn -> {-1, reset} end, name: name)
   end
 
@@ -22,27 +24,29 @@ defmodule PropCheck.Test.Counter do
   end
 
   @spec clear() :: :ok
-  @spec clear(pid) :: :ok
+  @spec clear(pid | atom) :: :ok
   def clear(pid \\ __MODULE__) do
     Agent.update(pid, fn {_, reset} -> {0, reset} end)
   end
 
   @spec get() :: integer
-  @spec get(pid) :: integer
+  @spec get(pid | atom) :: integer
   def get(pid \\ __MODULE__) do
     Agent.get(pid, fn {count, _reset} -> count end)
   end
 
   @spec inc() :: :integer
-  @spec inc(pid) :: :integer
-  @spec inc(pos_integer, pid) :: :integer
-  def inc(increment \\ 1, pid \\ __MODULE__) when increment > 0 do
+  @spec inc(pid | atom) :: :integer
+  def inc(pid \\ __MODULE__), do: add(1, pid)
+
+  @spec add(pos_integer, pid | atom) :: :integer
+  def add(increment, pid \\ __MODULE__) when increment > 0 do
     Agent.get_and_update(pid, fn
       {count, :infinity} ->
         new_count = count + increment
         {new_count, {new_count, :infinity}}
       {count, reset} ->
-        new_count =Integer.mod(count + increment, reset)
+        new_count = Integer.mod(count + increment, reset)
         {new_count, {new_count, reset}}
     end)
   end
