5d059132c6fac41afeaf9f348992db2da78600e1
Klaus Alfert <klaus.alfert@gmail.com>
2018-07-22 12:55:23 +0200

    Stacks used to employ auto-generators. Now they are explicit.

--- test/stack_test.exs
+++ test/stack_test.exs
@@ -3,23 +3,18 @@ defmodule PropCheck.StackTypeTest do
   use PropCheck
   alias PropCheck.Test.Stack
 
-
-  test "all Stack specs" do
-    #{_, failures} = PropCheck.check_specs(Stack, skip_mfas: [{Stack, :__info__, 1}])
-    #assert length(failures) == 0
-    IO.puts "Spec checking does not work"
-  end
-
+  # Generator for Stacks
+  def stack(gen), do: list(gen)
 
   property "pop(push) = original" do
-    forall {s, x} in {Stack.stack(integer()), integer()} do
+    forall [s, x] <- [stack(integer()), integer()] do
       {_y, t} = s |> Stack.push(x) |> Stack.pop
       s == t
     end
   end
 
   property "push make a stack bigger" do
-    forall {s, x} in {Stack.stack(integer()), integer()} do
+    forall [s, x] <- [stack(integer()), integer()] do
       (Stack.size(s) + 1) == Stack.size(s |> Stack.push(x))
     end
   end
