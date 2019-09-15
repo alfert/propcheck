477e1d045ef467c98138beb5a5c11451b229766b
X4lldux <x4lldux@vectron.io>
2019-09-15 14:24:19 +0200

    Test setting of module's default options

--- test/properties_test.exs
+++ test/properties_test.exs
@@ -1,6 +1,6 @@
 defmodule PropertiesTest do
   use ExUnit.Case
-  use PropCheck
+  use PropCheck, default_opts: [numtests: 1]
 
   setup do
     [generator: nat()]
@@ -10,5 +10,9 @@ defmodule PropertiesTest do
     forall n <- context.generator do
       is_integer(n)
     end
+  end
+
+  property "default options are set", [max_size: 1] do
+    assert Process.get(:property_opts) == [max_size: 1, numtests: 1]
   end
 end
