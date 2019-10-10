7f422750d50ab252e1bf531513702a836ee0c327
Klaus Alfert <klaus.alfert@gmail.com>
2019-10-10 20:37:35 +0200

    Prepare for releae (cont'd)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PropCheck.Mixfile do
 
   def project do
     [app: :propcheck,
-     version: "1.1.6-dev",
+     version: "1.2.0-dev",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
