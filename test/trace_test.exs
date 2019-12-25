17362dd1a219ec8285f148439712e8bf17493fce
Klaus Alfert <klaus.alfert@gmail.com>
2019-12-25 20:21:43 +0100

    first tests for the tracer

---
+++ test/trace_test.exs
@@ -0,0 +1,35 @@
+defmodule PropCheck.TracingTest do
+  @moduledoc """
+  Tests for Tracing sequences
+  """
+  use ExUnit.Case, async: true
+
+  use PropCheck, default_opts: &PropCheck.TestHelpers.config/0
+  import PropCheck.TestHelpers, except: [config: 0]
+
+  defmodule TracedModule do
+    use PropCheck.Tracer.Instrument
+
+    def hi, do: :hello
+
+    def put_hello do
+      IO.puts "Putting the hello"
+      send(self(), hi())
+    end
+
+    def get_hello do
+      receive do
+        :hello -> IO.puts "Received hello"
+          :hello
+      end
+
+    end
+  end
+
+  test "" do
+    GenServer.start_link(PropCheck.Tracer.Scheduler, :nothing, name: PropCheck.Tracer.Scheduler)
+    TracedModule.put_hello()
+    assert :hello == TracedModule.get_hello()
+  end
+
+end
