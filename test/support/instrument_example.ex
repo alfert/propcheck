b140f490dfee8531ab96602218946acf9b02ef9b
Klaus Alfert <klaus.alfert@gmail.com>
2020-04-04 22:34:46 +0200

    an example for instrumentation

---
+++ test/support/instrument_example.ex
@@ -0,0 +1,31 @@
+defmodule PropCheck.Support.InstrumentExample do
+
+  @moduledoc """
+  A module for instrumentation.
+  """
+  def hello(s) when is_binary(s) do
+    IO.puts "Hello to #{s}"
+    private_hello(s)
+  end
+
+  def fetch_from_ets(table, key) do
+    [x] = :ets.lookup_element(table, key, 1)
+    x
+  end
+
+  def put_to_ets(table, key, value) do
+    :ets.update_element(table, key, value)
+  end
+
+  def ets_in_expr(table, key) do
+    case :ets.lookup_element(table, key, 1) do
+      [] -> "empty list"
+      l when is_list(l) -> "a value list"
+      v -> "a single value"
+    end
+  end
+
+  defp private_hello(s) do
+    IO.puts "Private Hello to #{s}"
+  end
+end
