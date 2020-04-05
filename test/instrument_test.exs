eed90d34f9cb3ce102115e2ad2550524cdfdbb4e
Klaus Alfert <klaus.alfert@gmail.com>
2020-04-05 20:35:17 +0200

    Do some log output and check for instrumentable functions

--- test/instrument_test.exs
+++ test/instrument_test.exs
@@ -4,16 +4,20 @@ defmodule PropCheck.Test.InstrumentTester do
   """
 
   use ExUnit.Case
-
+  import ExUnit.CaptureLog
   alias PropCheck.Instrument
 
   defmodule Identity do
     @moduledoc """
     Implements the Instrumentation as the identity function
     """
+    require Logger
     @behaviour Instrument
     @impl true
-    def handle_function_call(call), do: IO.inspect(call)
+    def handle_function_call(call) do
+      Logger.error("handle_function: #{inspect call}")
+      call
+    end
   end
 
   test "Read the forms of the beam" do
@@ -22,12 +26,19 @@ defmodule PropCheck.Test.InstrumentTester do
     assert tuple_size(forms) == 2
     {:abstract_code, code} = forms
     assert tuple_size(code) == 2
-    {raw_abstract_v1, clauses} = code
+    assert {:raw_abstract_v1, clauses} = code
   end
 
   test "Initial instrumentation is the identity" do
     {:ok, forms} = Instrument.get_forms_of_module(PropCheck.Support.InstrumentExample)
-    instrumented_forms = Instrument.instrument_form(Identity, forms)
+    logs = capture_log fn ->
+      instrumented_forms = Instrument.instrument_form(Identity, forms)
+      assert forms == instrumented_forms
+    end
+    assert logs =~ "handle_function:"
   end
 
+  test "instrumentable functions" do
+    assert true == Instrument.instrumentable_function({:atom, 0, :gen_server}, {:atom, 0, :start_link})
+  end
 end
