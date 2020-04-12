874b937b2d936a22993eafb36c504e15180da3f7
Klaus Alfert <klaus.alfert@gmail.com>
2020-04-12 17:33:40 +0200

    add module compilation
    Handles also a custom attribute. But it does not work yet. But why?

--- lib/instrument.ex
+++ lib/instrument.ex
@@ -13,15 +13,38 @@ defmodule PropCheck.Instrument do
   @callback handle_function_call(call :: any) :: any
 
   @doc """
+  A callback to decide if the function `mod:fun` with any arity is a candidate
+  for instrumentation. The default implementation is simply calling
+  `instrumentable_function/2`.
+  """
+  @callback is_instrumentable_function(mod ::module, fun :: atom) :: boolean
+
+  @doc """
   Takes the object code of the module, instruments it and update the module
   in the code server with instrumented byte code.
   """
-  def instrument_module(mod) when is_atom(mod) do
+  @spec instrument_module(mod :: module, instrumeter :: module) :: any
+  def instrument_module(mod, instrumenter) when is_atom(mod) and is_atom(instrumenter) do
     # 1. Get the forms of the mod
-    # 2. instruments the forms of the mod
-    # 3. Compile them with :compile.forms
-    :not_implemented
+    {:ok, filename, forms} = get_forms_of_module(mod)
+    case is_instrumented?(forms) do
+      false ->
+        # 2. instruments the forms of the mod
+        {:abstract_code, {:raw_abstract_v1, altered_forms}} = instrument_forms(instrumenter, forms)
+        # 3. Compile them with :compile.forms
+        instr_attribute = {:attribute, 1, :instrumented, PropCheck}
+        instrumented_form = add_attribute([instr_attribute], altered_forms)
+        compile_module(mod, filename, instrumented_form)
+      attribute ->
+        Logger.error "Module #{inspect mod} is alread instrumented: #{inspect attribute}"
+    end
   end
+
+  def add_attribute(attrs, forms), do: add_attribute(attrs, [], forms)
+  def add_attribute([], forms, []), do: Enum.reverse(forms)
+  def add_attribute([], fs, [f | tail]), do: add_attribute([], [f | fs], tail)
+  def add_attribute(as, fs1, [{:attribute, _, _, _} = a | tail]), do: add_attribute(as, [a | fs1], tail)
+  def add_attribute([attr | as], fs1, tail), do: add_attribute(as, [attr | fs1], tail)
 
   @doc """
   Retrieves the abstract code, i.e. the list of forms, of the given
@@ -40,6 +63,9 @@ defmodule PropCheck.Instrument do
   the VM.
   """
   def compile_module(mod, filename, {:abstract_code, {:raw_abstract_v1, clauses}} = _abstract_code) do
+    compile_module(mod, filename, clauses)
+  end
+  def compile_module(mod, filename, clauses) when is_list(clauses) do
     options = [:binary, :debug_info, :return, :verbose]
     case :compile.noenv_forms(clauses, options) do
       {:ok, ^mod, bin_code, warnings} ->
@@ -51,6 +77,15 @@ defmodule PropCheck.Instrument do
     end
   end
 
+  @doc "Checks if the code is already instrumented. If not, returns `false` otherwise returns the attribute"
+  def is_instrumented?({:abstract_code, {:raw_abstract_v1, clauses}}), do: is_instrumented?(clauses)
+  def is_instrumented?(clauses) when is_list(clauses) do
+    Enum.find(clauses, false, fn
+      {:attribute, _line, :instrumented, _} -> true
+      _ -> false
+    end)
+  end
+
   # Helper function for passing the module through the mapping process
   defp map(enum, mod, fun) when is_function(fun, 2) do
     Enum.map(enum, &(fun.(mod, &1)))
@@ -58,148 +93,148 @@ defmodule PropCheck.Instrument do
   # Helper function for mapping expressions
   defp map_expr(enum, mod), do: map(enum, mod, &instrument_expr/2)
 
-  @doc "Instruments the form of a module"
-  def instrument_form(mod, {:abstract_code, {:raw_abstract_v1, clauses}}) when is_list(clauses) do
-    instr_clauses = map(clauses, mod, &instrument_mod_clause/2)
+  @doc "Instruments the forms of a module"
+  def instrument_forms(instrumenter, {:abstract_code, {:raw_abstract_v1, clauses}}) when is_list(clauses) do
+    instr_clauses = map(clauses, instrumenter, &instrument_mod_clause/2)
     {:abstract_code,
       {:raw_abstract_v1,
         instr_clauses}}
   end
 
   @doc "Instruments the clauses of a module"
-  def instrument_mod_clause(mod, {:function, line, name, arg_count, body}) do
-    instr_body = map(body, mod, &instrument_body/2)
+  def instrument_mod_clause(instrumenter, {:function, line, name, arg_count, body}) do
+    instr_body = map(body, instrumenter, &instrument_body/2)
     {:function, line, name, arg_count, instr_body}
   end
-  def instrument_mod_clause(_mod, clause), do: clause
+  def instrument_mod_clause(_instrumenter, clause), do: clause
 
   @doc "Instruments the each body (a `:clause`) of a function"
-  def instrument_body(mod, {:clause, line, args, local_vars, exprs}) do
-    instr_exprs = map_expr(exprs, mod)
+  def instrument_body(instrumenter, {:clause, line, args, local_vars, exprs}) do
+    instr_exprs = map_expr(exprs, instrumenter)
     {:clause, line, args, local_vars, instr_exprs}
   end
 
   @doc "This is a big switch over all kinds of expressions for instrumenting them"
-  def instrument_expr(_mod, {:atom, _, _} = a), do: a
-  def instrument_expr(mod, {:bc, line, expr, qs}) do
-    {:bc, line, instrument_expr(mod, expr), map(qs, mod, &instrument_qualifier/2)}
+  def instrument_expr(_instrumenter, {:atom, _, _} = a), do: a
+  def instrument_expr(instrumenter, {:bc, line, expr, qs}) do
+    {:bc, line, instrument_expr(instrumenter, expr), map(qs, instrumenter, &instrument_qualifier/2)}
   end
-  def instrument_expr(mod, {:bin, line, bin_elements}) do
-    {:bin, line, map(bin_elements, mod, &instrument_bin_element/2)}
+  def instrument_expr(instrumenter, {:bin, line, bin_elements}) do
+    {:bin, line, map(bin_elements, instrumenter, &instrument_bin_element/2)}
   end
-  def instrument_expr(mod, {:block, line, exprs}) do
-    {:block, line, map_expr(exprs, mod)}
+  def instrument_expr(instrumenter, {:block, line, exprs}) do
+    {:block, line, map_expr(exprs, instrumenter)}
   end
-  def instrument_expr(mod, {:case, line, expr, clauses}) do
-    instr_expr = instrument_expr(mod, expr)
-    {:case, line, instr_expr, map(clauses, mod, &instrument_clause/2)}
+  def instrument_expr(instrumenter, {:case, line, expr, clauses}) do
+    instr_expr = instrument_expr(instrumenter, expr)
+    {:case, line, instr_expr, map(clauses, instrumenter, &instrument_clause/2)}
   end
-  def instrument_expr(mod, {:catch, line, expr}), do: {:catch, line, instrument_expr(mod, expr)}
-  def instrument_expr(mod, {:cons, line, e1, e2}), do: {:cons, line, instrument_expr(mod, e1), instrument_expr(mod, e2)}
-  def instrument_expr(mod, {:fun, line, cs}) when is_list(cs) do
-    {:fun, line, map(cs, mod, &instrument_clause/2)}
+  def instrument_expr(instrumenter, {:catch, line, expr}), do: {:catch, line, instrument_expr(instrumenter, expr)}
+  def instrument_expr(instrumenter, {:cons, line, e1, e2}), do: {:cons, line, instrument_expr(instrumenter, e1), instrument_expr(instrumenter, e2)}
+  def instrument_expr(instrumenter, {:fun, line, cs}) when is_list(cs) do
+    {:fun, line, map(cs, instrumenter, &instrument_clause/2)}
   end
-  def instrument_expr(_mod, {:fun, _, _} = f), do: f
-  def instrument_expr(mod, {:call, _l, {:remote, _m, _f}, _args} = c), do: instrument_function_call(mod, c)
-  def instrument_expr(mod, {:call, _l, _f, _args} = c), do: instrument_function_call(mod, c)
-  def instrument_expr(mod, {:if, line, cs}), do: {:if, line, map(cs, mod, &instrument_clause/2)}
-  def instrument_expr(mod, {:lc, line, e, qs}) do
-    {:lc, line, instrument_expr(mod, e), map(qs, mod, &instrument_qualifier/2)}
+  def instrument_expr(_instrumenter, {:fun, _, _} = f), do: f
+  def instrument_expr(instrumenter, {:call, _l, {:remote, _m, _f}, _args} = c), do: instrument_function_call(instrumenter, c)
+  def instrument_expr(instrumenter, {:call, _l, _f, _args} = c), do: instrument_function_call(instrumenter, c)
+  def instrument_expr(instrumenter, {:if, line, cs}), do: {:if, line, map(cs, instrumenter, &instrument_clause/2)}
+  def instrument_expr(instrumenter, {:lc, line, e, qs}) do
+    {:lc, line, instrument_expr(instrumenter, e), map(qs, instrumenter, &instrument_qualifier/2)}
   end
-  def instrument_expr(mod, {:map, line, assocs}), do: {:map, line, map(assocs, mod,  &instrument_assoc/2)}
-  def instrument_expr(mod, {:map, line, expr, assocs}) do
-    {:map, line, instrument_expr(mod, expr), map(assocs, mod, &instrument_assoc/2)}
+  def instrument_expr(instrumenter, {:map, line, assocs}), do: {:map, line, map(assocs, instrumenter,  &instrument_assoc/2)}
+  def instrument_expr(instrumenter, {:map, line, expr, assocs}) do
+    {:map, line, instrument_expr(instrumenter, expr), map(assocs, instrumenter, &instrument_assoc/2)}
   end
-  def instrument_expr(mod, {:match, line, p, e}) do
-    {:match, line, instrument_pattern(mod, p), instrument_expr(mod, e)}
+  def instrument_expr(instrumenter, {:match, line, p, e}) do
+    {:match, line, instrument_pattern(instrumenter, p), instrument_expr(instrumenter, e)}
   end
-  def instrument_expr(_mod, {:nil, line}), do: {:nil, line}
-  def instrument_expr(mod, {:op, line, op, e1}), do: {:op, line, op, instrument_expr(mod, e1)}
-  def instrument_expr(mod, {:op, line, op, e1, e2}), do:
-    {:op, line, op, instrument_expr(mod, e1), instrument_expr(mod, e2)}
-  def instrument_expr(mod, {:receive, _line, _cs} = r), do: instrument_receive(mod, r)
-  def instrument_expr(mod, {:receive, _line, _cs, _e, _b} = r), do: instrument_receive(mod, r)
-  def instrument_expr(mod, {:record, line, name, fields}) do # record creation
-    {:record, line, name, map_expr(fields, mod)}
+  def instrument_expr(_instrumenter, {:nil, line}), do: {:nil, line}
+  def instrument_expr(instrumenter, {:op, line, op, e1}), do: {:op, line, op, instrument_expr(instrumenter, e1)}
+  def instrument_expr(instrumenter, {:op, line, op, e1, e2}), do:
+    {:op, line, op, instrument_expr(instrumenter, e1), instrument_expr(instrumenter, e2)}
+  def instrument_expr(instrumenter, {:receive, _line, _cs} = r), do: instrument_receive(instrumenter, r)
+  def instrument_expr(instrumenter, {:receive, _line, _cs, _e, _b} = r), do: instrument_receive(instrumenter, r)
+  def instrument_expr(instrumenter, {:record, line, name, fields}) do # record creation
+    {:record, line, name, map_expr(fields, instrumenter)}
   end
-  def instrument_expr(mod, {:record, line, e, name, fields}) do # record update
-    {:record, line, instrument_expr(mod, e), name, map_expr(fields, mod)}
+  def instrument_expr(instrumenter, {:record, line, e, name, fields}) do # record update
+    {:record, line, instrument_expr(instrumenter, e), name, map_expr(fields, instrumenter)}
   end
-  def instrument_expr(mod, {:record_field, line, field, expr}), do: {:record, line, field, instrument_expr(mod, expr)}
-  def instrument_expr(mod, {:record_field, line, expr, name, field}), do:
-    {:record, line, instrument_expr(mod, expr), name, field}
-  def instrument_expr(_mod, {:record_index, _line, _name, _fields} = r), do: r
-  def instrument_expr(mod, {:tuple, line, es}), do: {:tuple, line, map_expr(es, mod)}
-  def instrument_expr(mod, {:try, line, body, cases, catches, expr}) do
-    i_body = map_expr(body, mod)
-    i_cases = map(cases, mod, &instrument_clause/2)
-    i_catches = map(catches, mod, &instrument_clause/2)
-    {:try, line, i_body, i_cases, i_catches, instrument_expr(mod, expr)}
+  def instrument_expr(instrumenter, {:record_field, line, field, expr}), do: {:record, line, field, instrument_expr(instrumenter, expr)}
+  def instrument_expr(instrumenter, {:record_field, line, expr, name, field}), do:
+    {:record, line, instrument_expr(instrumenter, expr), name, field}
+  def instrument_expr(_instrumenter, {:record_index, _line, _name, _fields} = r), do: r
+  def instrument_expr(instrumenter, {:tuple, line, es}), do: {:tuple, line, map_expr(es, instrumenter)}
+  def instrument_expr(instrumenter, {:try, line, body, cases, catches, expr}) do
+    i_body = map_expr(body, instrumenter)
+    i_cases = map(cases, instrumenter, &instrument_clause/2)
+    i_catches = map(catches, instrumenter, &instrument_clause/2)
+    {:try, line, i_body, i_cases, i_catches, instrument_expr(instrumenter, expr)}
   end
-  def instrument_expr(_mod, {:var, _l, _name} = v), do: v
-  def instrument_expr(_mod, {literal, _line, _val} = l) when literal in [:atom, :integer, :float, :char, :string], do: l
+  def instrument_expr(_instrumenter, {:var, _l, _name} = v), do: v
+  def instrument_expr(_instrumenter, {literal, _line, _val} = l) when literal in [:atom, :integer, :float, :char, :string], do: l
 
   @doc "Instrument a part of binary pattern definition"
-  def instrument_bin_element(mod, {:bin_element, line, expr, size, tsl}) do
-    {:bin_element, line, instrument_expr(mod, expr), size, tsl}
+  def instrument_bin_element(instrumenter, {:bin_element, line, expr, size, tsl}) do
+    {:bin_element, line, instrument_expr(instrumenter, expr), size, tsl}
   end
 
   @doc "Instrument case, catch, function clauses"
-  def instrument_clause(mod, {:clause, line, p, body}) do
-    {:clause, line, instrument_pattern(mod, p), map_expr(body, mod)}
+  def instrument_clause(instrumenter, {:clause, line, p, body}) do
+    {:clause, line, instrument_pattern(instrumenter, p), map_expr(body, instrumenter)}
   end
-  # def instrument_clause(mod, {:clause, line, ps, body}) when is_list(ps) do
-  #   {:clause, line, map_expr(ps, mod), map_expr(body, mod)}
+  # def instrument_clause(instrumenter, {:clause, line, ps, body}) when is_list(ps) do
+  #   {:clause, line, map_expr(ps, instrumenter), map_expr(body, instrumenter)}
   # end
-  def instrument_clause(mod, {:clause, line, ps, [guards], body}) when is_list(ps) and is_list(guards) do
-    {:clause, line, map_expr(ps, mod), [map_expr(guards, mod)], map_expr(body, mod)}
+  def instrument_clause(instrumenter, {:clause, line, ps, [guards], body}) when is_list(ps) and is_list(guards) do
+    {:clause, line, map_expr(ps, instrumenter), [map_expr(guards, instrumenter)], map_expr(body, instrumenter)}
   end
-  def instrument_clause(mod, {:clause, line, ps, guards, body}) when is_list(ps) do
-    {:clause, line, map_expr(ps, mod), map_expr(guards, mod), map_expr(body, mod)}
+  def instrument_clause(instrumenter, {:clause, line, ps, guards, body}) when is_list(ps) do
+    {:clause, line, map_expr(ps, instrumenter), map_expr(guards, instrumenter), map_expr(body, instrumenter)}
   end
 
   @doc "Instrument qualifiers of list and bit comprehensions"
-  def instrument_qualifier(mod, {:generate, line, p, e}), do: {:generate, line, instrument_pattern(mod, p), instrument_expr(mod, e)}
-  def instrument_qualifier(mod, {:b_generate, line, p, e}), do: {:b_generate, line, instrument_pattern(mod, p), instrument_expr(mod, e)}
-  def instrument_qualifier(mod, e), do: instrument_expr(mod, e)
+  def instrument_qualifier(instrumenter, {:generate, line, p, e}), do: {:generate, line, instrument_pattern(instrumenter, p), instrument_expr(instrumenter, e)}
+  def instrument_qualifier(instrumenter, {:b_generate, line, p, e}), do: {:b_generate, line, instrument_pattern(instrumenter, p), instrument_expr(instrumenter, e)}
+  def instrument_qualifier(instrumenter, e), do: instrument_expr(instrumenter, e)
 
   @doc "Instrument patterns, which are mostly expressions, except for variables/atoms"
-  # def instrument_pattern(mod, {x, p, s}), do: {x, instrument_expr(mod, p), s}
-  def instrument_pattern(mod, ps) when is_list(ps), do: map(ps, mod, &instrument_pattern/2)
-  def instrument_pattern(mod, p), do: instrument_expr(mod, p)
+  # def instrument_pattern(instrumenter, {x, p, s}), do: {x, instrument_expr(instrumenter, p), s}
+  def instrument_pattern(instrumenter, ps) when is_list(ps), do: map(ps, instrumenter, &instrument_pattern/2)
+  def instrument_pattern(instrumenter, p), do: instrument_expr(instrumenter, p)
 
-  def instrument_assoc(mod, {assoc, line, key, value}) do
-    {assoc, line, instrument_expr(mod, key), instrument_expr(mod, value)}
+  def instrument_assoc(instrumenter, {assoc, line, key, value}) do
+    {assoc, line, instrument_expr(instrumenter, key), instrument_expr(instrumenter, value)}
   end
 
   @doc """
-  Instruments a function call and gives control the handler module `mod`.
+  Instruments a function call and gives control the handler module `instrumenter`.
   For now, we only instrumenting a call, if it is any of the interesting functions,
   i.e those might be a source of concurrency problems due to a shared mutable
   state or otherwise tinkering with scheduling.
   """
-  def instrument_function_call(mod, {:call, line, {:remote, line2, m, f}, as}) do
-    module = instrument_expr(mod, m)
-    fun = instrument_expr(mod, f)
-    args = map_expr(as, mod)
-    case instrumentable_function(m, f) do
-      true -> mod.handle_function_call({:call, line, {:remote, line2, module, fun}, args})
+  def instrument_function_call(instrumenter, {:call, line, {:remote, line2, m, f}, as}) do
+    module = instrument_expr(instrumenter, m)
+    fun = instrument_expr(instrumenter, f)
+    args = map_expr(as, instrumenter)
+    case instrumenter.is_instrumentable_function(m, f) do
+      true -> instrumenter.handle_function_call({:call, line, {:remote, line2, module, fun}, args})
       _ -> {:call, line, {:remote, line2, module, fun}, args}
     end
   end
-  def instrument_function_call(mod, {:call, line, f, as}) do
-    fun = instrument_expr(mod, f)
-    args = map_expr(as, mod)
+  def instrument_function_call(instrumenter, {:call, line, f, as}) do
+    fun = instrument_expr(instrumenter, f)
+    args = map_expr(as, instrumenter)
     {:call, line, fun, args}
   end
 
   @doc "The receive might be handled differently, therefore it has its own function"
-  def instrument_receive(mod, {:receive, line, cs}) do
-    {:receive, line, map(cs, mod, &instrument_clause/2)}
+  def instrument_receive(instrumenter, {:receive, line, cs}) do
+    {:receive, line, map(cs, instrumenter, &instrument_clause/2)}
   end
-  def instrument_receive(mod, {:receive, line, cs, e, b})  do
-    {:receive, line, map(cs, mod, &instrument_clause/2),
-      instrument_expr(mod, e), map_expr(b, mod)}
+  def instrument_receive(instrumenter, {:receive, line, cs, e, b})  do
+    {:receive, line, map(cs, instrumenter, &instrument_clause/2),
+      instrument_expr(instrumenter, e), map_expr(b, instrumenter)}
   end
 
   @doc """
