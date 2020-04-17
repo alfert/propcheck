5999514f66aaa78e25c2ad8fc5db0043bce48e08
Klaus Alfert <klaus.alfert@gmail.com>
2020-04-17 17:20:03 +0200

    new test for concurrency testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
 ExUnit.start()
 ExUnit.configure(exclude: [will_fail: true, unstable_test: true, manual: true,
-                           not_implemented: true])
+                           not_implemented: true, concurrency_test: true])
