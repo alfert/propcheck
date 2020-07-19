03dfd034422a020da5281679eb125765e417a04e
Klaus Alfert <klaus.alfert@gmail.com>
2020-07-19 16:35:00 +0200

    bump version to 1.2.2-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PropCheck.Mixfile do
 
   def project do
     [app: :propcheck,
-     version: "1.2.1",
+     version: "1.2.2-dev",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
