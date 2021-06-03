7b473a0762619ccd61da7400869923df700efd8d
Magnus Ottenklinger <evnu@posteo.de>
2021-06-03 14:22:33 +0200

    Add .formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,12 @@
+[
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    defcommand: 2,
+    implies: 2,
+    let: 2,
+    forall: 2,
+    forall: 3,
+    such_that: 2,
+    such_that_maybe: 2
+  ]
+]
