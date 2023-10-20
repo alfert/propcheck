e715cf3b1c1a4f2b5c0c251e311b62a85ca5d516
Magnus Ottenklinger <evnu@posteo.de>
2023-10-20 21:55:18 +0200

    deps: remove non-functional coverex

--- mix.exs
+++ mix.exs
@@ -86,7 +86,6 @@ defmodule PropCheck.Mixfile do
     [
       {:proper, github: "proper-testing/proper", ref: "a5ae5669f01143b0828fc21667d4f5e344aa760b"},
       {:libgraph, "~> 0.13"},
-      {:coverex, "~> 1.4", only: :test},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev}
     ]
