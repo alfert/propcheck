b81a8dcdf70d18945eca279a2190a06a658e9004
Magnus Ottenklinger <evnu@posteo.de>
2023-10-20 21:27:56 +0200

    deps: update versions

--- mix.exs
+++ mix.exs
@@ -93,7 +93,7 @@ defmodule PropCheck.Mixfile do
       {:libgraph, "~> 0.13"},
       {:coverex, "~> 1.4", only: :test},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev}
     ]
   end
